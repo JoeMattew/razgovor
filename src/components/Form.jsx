36f99eae0c509ac13d18754e076032932fa191f1
Marat Saitov <maratsaitov@Marats-MacBook-Air.local>
2024-10-17 14:29:28 +0700

    form fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Form = () => {
   return (
+    
     <div className="w-full max-w-3xl mx-auto px-6 py-28" id="omnisend-embedded-v2-670a46ea518f7d3017ccebbd"></div>
+   
   );
 };
 
